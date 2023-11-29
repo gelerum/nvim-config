3cdb468a7def978a1dbb52bf35aecd7b2bdd983d
Gleb Gonchar <gelerum@gmail.com>
2023-11-29 19:43:34 +0700

    right angles

--- lua/core/configs/lualine.lua
+++ lua/core/configs/lualine.lua
@@ -2,19 +2,19 @@ require('lualine').setup {
     options = {
         disabled_filetypes   = { 'packer', 'NvimTree' },
         globalstatus         = false,
-        section_separators   = { left = '', right = '' },
+        section_separators   = { left = '', right = '' },
         component_separators = '',
     },
     sections = {
         lualine_a = {
-            { 'mode', separator = { right = '' }, },
+            { 'mode', separator = { right = '' } },
         },
         lualine_b = { 'branch', 'diff' },
         lualine_c = { { "filename", path = 3 } },
         lualine_x = {},
         lualine_y = { 'diagnostics', 'progress' },
         lualine_z = {
-            { 'location', separator = { left = '' }, },
+            { 'location', separator = { left = '' }, },
         },
     },
 }
